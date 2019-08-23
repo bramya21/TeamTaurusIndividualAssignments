43507dadd6fd3f2719af4f314d87871e2877fa69
unknown <raminisai33@gmail.com>
2019-08-23 08:33:59 +0530

    first assignment

---
+++ saikrishna/movie-management/movie.model.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var Movie = /** @class */ (function () {
+    function Movie(id, title, description, production) {
+        this.id = id;
+        this.description = description;
+        this.production = production;
+        this.title = title;
+    }
+    return Movie;
+}());
+exports.Movie = Movie;
---
+++ saikrishna/movie-management/movie.test.ts
@@ -0,0 +1,16 @@
+import {MovieImple} from './movie.implementaion'
+import {Movie} from './movie.model'
+
+var mv:Movie[];
+var movieObject:Movie=new Movie(
+    1,
+    "a hero never dies",
+    "fantasy...",
+    "fox"
+)
+mv = [movieObject]
+console.log(movieObject.title)
+var mvImp = new MovieImple(mv)
+// mvImp.setMovie(movieObject);
+console.log(mvImp.getMovies()[0]);
+console.log(mvImp.getMovieByName("a hero never dies"))
