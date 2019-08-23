b81da3ef7a8a890612fd4ee08d94e7eaf86f26b7
sruthi98 <30915672+sruthi98@users.noreply.github.com>
2019-08-23 09:19:30 +0530

    Create Customer.ts

---
+++ sruthiv/Customer.ts
@@ -0,0 +1,10 @@
+import {Theater} from './Theater';
+export class Customer{
+    viewMovies(){
+        Theater.listMovies();
+    }
+
+    makeBooking(movieName){
+        Theater.confirmBooking('one');
+    }
+}
