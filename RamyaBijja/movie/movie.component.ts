b826d8be9e584de20e0c02d0936df3ba2a2f6e95
bramya21 <31098382+bramya21@users.noreply.github.com>
2019-08-24 09:15:18 +0530

    Add files via upload
    23-08-2019

---
+++ RamyaBijja/movie/movie.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Movie} from '../movie'
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+  public obj: Movie;
+  constructor() {
+    this.obj=new Movie();
+   }
+ 
+  ngOnInit() {
+  }
+
+}
